65ed99eaa5ddb52de318acd5f7caaa48cd59ef09
16808910041680 <forforfirefire@gmail.com>
2025-05-11 11:02:09 +1000

    h

---
+++ hius.py
@@ -0,0 +1,16 @@
+def random_def() :
+    print ("This is a function that tells you what you should wear my temperature, place and time of the day.")
+    print (input ("Please enter the temperature in Celsius: "))
+    print (input ("Please enter the place you are going to: "))
+    print (input ("Please enter the time of the day: "))
+    if int(input) < 20 :
+        print ("You should wear a coat.")
+    elif int(input) < 30 :
+        print ("You should wear a t-shirt.")
+    elif int(input) < 40 :
+        print ("You should wear a t-shirt and shorts.")
+    else :
+        print ("You should wear a swimsuit. To be embarrasing. IDK") 
+    print ("Have a nice day!")
+    
+
